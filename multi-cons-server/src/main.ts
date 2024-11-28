1a46e4dbff84877d29f984b4d7320bd8677cd18e
RomanLesovoy <romadesire97@gmail.com>
2024-11-28 13:46:46 +0200

    cors url

--- multi-cons-server/src/main.ts
+++ multi-cons-server/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({ origin: 'https://multi-cons.web.app' });
 
   await app.listen(3000);
 }
